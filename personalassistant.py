aefea9d9c59993a77c1bf1493c8d27b50655e6be
bmwayne <rohitvarmachitti@gmail.com>
2021-01-23 19:34:33 -0600

    Personal Assistant

--- personalassistant.py
+++ personalassistant.py
@@ -1,33 +1,44 @@
 import wolframalpha
 import wikipedia
-import tkinter
+from tkinter import *
 import speech_recognition as sr
 
 while True:
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        print('Say Something...')
         audio = r.listen(source)
 
         try:
+            print('Recognizing...')
             text = r.recognize_google(audio)
+            print(text)
             if text == 'stop':
                 print('Thank you using my Personal Assistant')
+                break
             else:
-                window = tkinter.Tk()
+                window = Tk()
                 window.geometry('600x600')
                 try:
                     wolfid = 'UY2U9Q-PKAY23Y5TJ'
                     client = wolframalpha.Client(wolfid)
-                    result = client.query(text)
-                    answer = next(result.results).text
+                    res = client.query(text)
+                    answer = next(res.results).text
+                    print('Viola! Got an answer from WolframAlpha')
+                    print(answer)
 
-                    label = Label(window, justify = LEFT, compound = CENTER, padx = 10, text = answer, font = 'times 15 bold' )
-                    label.pack()
-                    tkinter.mainloop()
+                    label1 = Label(window, justify = LEFT, compound = CENTER, padx = 10, text = answer, font = 'times 15 bold' )
+                    label1.pack()
+                    window.after(5000, lambda :window.destroy())
+                    window.mainloop()
                 except:
                     print('WolframAlpha could not find anything. Let\'s try Wikipedia!')
-                    answer = wikipedia.summary(text)
+                    wikianswer = wikipedia.summary(text)
 
-                    label = Label(window, justify=LEFT, compound=CENTER, padx=10, text=answer, font='times 15 bold')
-                    label.pack()
-                    tkinter.mainloop()
+                    label1 = Label(window, justify=LEFT, compound=CENTER, padx=10, text=wikianswer, font='times 15 bold')
+                    label1.pack()
+                    window.after(50000, lambda: window.destroy())
+                    window.mainloop()
+        except:
+            answer = 'Sorry I zoned out.. What did you say again?'
+            print(answer)
