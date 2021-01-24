55b45e6015315bf8a58b045f249ccd19c00b7ce0
bmwayne <rohitvarmachitti@gmail.com>
2021-01-23 18:52:17 -0600

    gave microphone as input for speech recognition

--- personalassistant.py
+++ personalassistant.py
@@ -1,14 +1,9 @@
 import wolframalpha
 import wikipedia
+import tkinter
 import speech_recognition as sr
-import pyaudio
-from tkinter import *
-import tkinter.messagebox
-
-wolfid = 'UY2U9Q-PKAY23Y5TJ'
 
 while True:
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
-#randomtext
